f429370ff1112238980e681d96f700701fbfdf8c
y.romanchuk <y.romanchuk@bi.zone>
2022-10-11 01:09:38 +0300

    small refactoring

--- check.py
+++ check.py
@@ -9,11 +9,11 @@ def power(x, y, p):
 
     # Update x if it is more than
     # or equal to p
-    while (y > 0):
+    while y > 0:
 
         # If y is odd, multiply
         # x with result
-        if (y & 1):
+        if y & 1:
             res = (res * x) % p
 
         # y must be even now
@@ -24,21 +24,20 @@ def power(x, y, p):
 
 # Returns true if there exists an integer
 # x such that (x*x)%p = n%p
-def squareRootExists(n, p):
+def square_root_exists(n, p):
     # Check for Euler's criterion that is
     # [n ^ ((p-1)/2)] % p is 1 or not.
-    if (power(n, (int)((p - 1) / 2), p) == 1):
+    if power(n, int((p - 1) / 2), p) == 1:
         return True
     return False
 
 
-# Driver Code
-p = 19
-n = 87463
-if (squareRootExists(n, p) == True):
-    print("Yes")
-else:
-    print("No")
-    print("Biba")
+if __name__ == "__main__":
 
-# This code is contributed by Rajput-Ji
+    # Driver Code
+    P, N = 19, 87463
+    if square_root_exists(N, P):
+        print("Yes")
+    else:
+        print("No")
+        print("Biba")
--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import math
+
 import numpy as np
-import scipy as sc
 import sympy as sy
+
 N = 87463
 
+
 def primfacs(n):
-   i = 2
-   primfac = []
-   while i * i <= n:
-       while n % i == 0:
-           primfac.append(i)
-           n = n / i
-       i = i + 1
-   if n > 1:
-       primfac.append(n)
-   return primfac
+    i = 2
+    primfac = []
+    while i * i <= n:
+        while n % i == 0:
+            primfac.append(i)
+            n = n / i
+        i = i + 1
+    if n > 1:
+        primfac.append(n)
+    return primfac
+
 
 def pow_two(p):
-    i = 2
     res = []
     n = p - 1
     S = 0
@@ -30,13 +28,14 @@ def pow_two(p):
         prom /= 2
         S += 1
     res.append(S)
-    Q = math.ceil(n / math.pow(2,S))
+    Q = math.ceil(n / math.pow(2, S))
     res.append(Q)
 
     return res
 
-def Tonelli_Shanks(n,p):
-    if not Eulers_Criteria(n,p):
+
+def tonelli_shanks(n, p):
+    if not eulers_criteria(n, p):
         return 0
     QS_arr = pow_two(p)
     n = n % p
@@ -45,39 +44,39 @@ def Tonelli_Shanks(n,p):
 
     z = 2
     while True:
-        if Eulers_Criteria(z,p):
+        if eulers_criteria(z, p):
             z += 1
         else:
             break
     M = S % p
-    c = int(math.pow(z,Q)) % p
-    t = int(math.pow(n,Q)) % p
+    c = int(math.pow(z, Q)) % p
+    t = int(math.pow(n, Q)) % p
     R = int(math.pow(n, int((Q + 1) / 2))) % p
 
     while t != 1:
         i = 0
         for j in range(1, M):
-            if int(math.pow(t, math.pow(2,j))) % p == 1:
+            if int(math.pow(t, math.pow(2, j))) % p == 1:
                 i = j
                 break
-        b = int(math.pow(c,math.pow(2,M - i - 1))) % p
+        b = int(math.pow(c, math.pow(2, M - i - 1))) % p
         M = i % p
-        c = int(math.pow(b,2)) % p
+        c = int(math.pow(b, 2)) % p
         t = t * c % p
-        R = R*b % p
+        R = R * b % p
 
-    return [R, (R*(-1)) % p]
+    return [R, (R * (-1)) % p]
 
 
-def Eulers_Criteria(n,p):
+def eulers_criteria(n, p):
     res = 1
     n = n % p
     pow = math.ceil((p - 1) / 2)
 
-    while(pow):
+    while pow:
         if pow & 1:
             res = (res * n) % p
-        n = (n*n) % p
+        n = (n * n) % p
         pow = pow >> 1
 
     if res == 1:
@@ -86,10 +85,11 @@ def Eulers_Criteria(n,p):
 
 
 def F_a(num):
-    return math.pow(num,2) - N #за модуль не выйдешь
+    return math.pow(num, 2) - N  # за модуль не выйдешь
     pass
 
-def build_factor_base(num): #Это дерьмо можно распарралелить
+
+def build_factor_base(num):  # Это дерьмо можно распарралелить
     a = [i for i in range(0, num + 1)]
     a[1] = 0
     lst = []
@@ -102,13 +102,14 @@ def build_factor_base(num): #Это дерьмо можно распаррале
         i += 1
     return lst
 
+
 def sieve_numb():
 
     m = 296
 
     A = math.ceil(pow(math.exp(math.sqrt(math.log(N) * math.log(math.log(N)))), 1 / math.sqrt(2))) + 1
     LS = []
-    for i in range(0,52):
+    for i in range(0, 52):
         LS.append(F_a(m + i))
     FB_prom = build_factor_base(A)
     FB = []
@@ -116,33 +117,31 @@ def sieve_numb():
         if i == 2:
             FB.append(i)
             continue
-        if Eulers_Criteria(N,i):
+        if eulers_criteria(N, i):
             FB.append(i)
-    get_all_root = []
 
     matrix = sy.zeros(len(LS), len(FB))
 
-
-    for i in range(0,len(FB)):
-        prom = Tonelli_Shanks(N,FB[i])
+    for i in range(len(FB)):
+        prom = tonelli_shanks(N, FB[i])
         index_one = (prom[0] - m) % FB[i]
         index_two = (prom[1] - m) % FB[i]
 
-        for j in range(0,len(LS)):
-                if index_one == j:
-                    while LS[j] % FB[i] == 0:
-                        LS[j] = LS[j] / FB[i]
-                        matrix[j,i] += 1
-                    index_one += FB[i]
-                if index_two == j:
-                    while LS[j] % FB[i] == 0:
-                        LS[j] = LS[j] / FB[i]
-                        matrix[j,i] += 1
-                    index_two += FB[i]
+        for j in range(0, len(LS)):
+            if index_one == j:
+                while LS[j] % FB[i] == 0:
+                    LS[j] = LS[j] / FB[i]
+                    matrix[j, i] += 1
+                index_one += FB[i]
+            if index_two == j:
+                while LS[j] % FB[i] == 0:
+                    LS[j] = LS[j] / FB[i]
+                    matrix[j, i] += 1
+                index_two += FB[i]
 
     print(matrix)
     prom = []
-    for i in range(0,len(LS)):
+    for i in range(0, len(LS)):
         if LS[i] == 1:
             prom.append(matrix.row(i))
             # print(i)
@@ -153,7 +152,7 @@ def sieve_numb():
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == "__main__":
     # print(pow_two(13))
     # sieve_numb()
     # matr = sy.Matrix([[0,0,0,1,0,0,0,0,0],
@@ -164,13 +163,17 @@ if __name__ == '__main__':
     #                   [1,1,1,0,0,0,0,0,1],
     #                   [1,1,0,0,1,0,0,0,0]])
     # print(matr.inv_mod(2))
-    matrix = np.matrix([ [0,0,0,1,0,0,0,0,0],
-                        [1,1,0,1,1,0,0,0,0],
-                        [0,1,0,0,0,0,1,0,1],
-                        [1,0,1,0,0,0,1,0,0],
-                        [0,0,0,1,0,0,0,0,0],
-                        [1,1,1,0,0,0,0,0,1],
-                        [1,1,0,0,1,0,0,0,0]])
+    matrix = np.matrix(
+        [
+            [0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [1, 1, 0, 1, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 1, 0, 1],
+            [1, 0, 1, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0, 0, 0, 1],
+            [1, 1, 0, 0, 1, 0, 0, 0, 0],
+        ]
+    )
     matrix = matrix.transpose()
     print(np.triu(matrix))
     # print(np.triu(    [ [0,0,0,1,0,0,0,0,0],
